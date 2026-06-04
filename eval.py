fb82e6958e5919893838c29f479f84d9d07deb91
MuhammadYasserGhifariTA <you@example.com>
2026-06-04 17:34:05 +0700

    init

--- eval.py
+++ eval.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO(r"runs\detect\runs\train\kaganga_v1-4\weights\best.pt")
+    model = YOLO(r"runs\detect\runs\train\kaganga_v1\weights\best.pt")
     metrics = model.val(data="datasets/data.yaml", split="val", imgsz=416, workers=0)
     print(f"mAP50     : {metrics.box.map50:.4f}")
     print(f"mAP50-95  : {metrics.box.map:.4f}")
